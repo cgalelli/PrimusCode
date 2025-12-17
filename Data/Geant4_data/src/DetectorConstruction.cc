b7521b1088907f8528eab054482e6fbe7cdb1d03
cgalelli <claudio.galelli@mi.infn.it>
2025-12-17 12:30:09 +0100

    Partial neutron files for olivine

--- Data/Geant4_data/src/DetectorConstruction.cc
+++ Data/Geant4_data/src/DetectorConstruction.cc
@@ -73,6 +73,11 @@ void DetectorConstruction::DefineMaterials()
   Water->AddElement(H, 2);
   Water->AddElement(O, 1);
 
+  G4Material* Quartz = new G4Material("StdRock",2.65*CLHEP::g/CLHEP::cm3,2, kStateSolid );
+  Quartz->AddElement(O,  67.*perCent);
+  Quartz->AddElement(Si, 33.*perCent);
+
+
   // --- Standard Materials (Optional) ---
   nist->FindOrBuildMaterial("G4_SiO2");
   nist->FindOrBuildMaterial("G4_AIR");
